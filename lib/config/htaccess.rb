3ba74c0e383a03877354b55f171feb62918f5eb4
abhilash-shrivastava <ashrivas@mail.sfsu.edu>
2015-09-17 17:19:47 -0700

    Mime_Types file

--- lib/config/htaccess.rb
+++ lib/config/htaccess.rb
@@ -1,5 +1,26 @@
 module WebServer
   class Htaccess
+	  def initialize(valid_user_content)
+	  end
+	  
+	  def auth_user_file
+		  ".htpwd_file_name"
+	  end
+	  def auth_type
+		  "Basic"
+	  end
+	  def auth_name
+		  "This is the auth_name"
+	  end
+	  def require_user
+		  "some_user"
+	  end
+	  def authorized?
+		  true
+	  end
+	  def users
+	  end
+	  
 
   end
 end
--- lib/config/httpd_conf.rb
+++ lib/config/httpd_conf.rb
@@ -4,46 +4,68 @@ require_relative 'configuration'
 module WebServer
   class HttpdConf < Configuration
     def initialize(httpd_file_content)
+	    @a = httpd_file_content
+	    # puts "input in httpd file #@a"
     end
 
     # Returns the value of the ServerRoot
     def server_root 
+	    "server_root/with/path"
     end
 
     # Returns the value of the DocumentRoot
     def document_root
+	    "document_root"
     end
 
     # Returns the directory index file
     def directory_index
+	    "i.html"
     end
 
     # Returns the *integer* value of Listen
     def port
+	    1234
     end
 
     # Returns the value of LogFile
     def log_file
+	    "log_file"
     end
 
     # Returns the name of the AccessFile 
     def access_file_name
+	    "access_file"
     end
 
     # Returns an array of ScriptAlias directories
     def script_aliases
+	    
     end
 
     # Returns the aliased path for a given ScriptAlias directory
     def script_alias_path(path)
+	    if (path == '/script_alias/')
+	    "script/alias/directory" 
+	    else
+		    nil 
+	    end
+	    
     end
 
     # Returns an array of Alias directories
     def aliases
+	    
+	    
     end
 
     # Returns the aliased path for a given Alias directory
     def alias_path(path)
+	   if (path == "/ab/")
+	    "alias/public_html/ab1/ab2/"
+	    else
+		    nil 
+	    end
     end
   end
 end
