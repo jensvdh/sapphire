dbf362e8aaf7f0f8f6003aa591123fe055d2f96d
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2015-09-19 16:40:52 -0700

    :more mimetype things

--- lib/config/mime_types.rb
+++ lib/config/mime_types.rb
@@ -4,30 +4,26 @@ require_relative "configuration"
 module WebServer
   class MimeTypes < Configuration
     def initialize(mime_file_content)
-	@mimetype = Hash.new
-	mimecontent = mime_file_content.split("\n")
-	mimecontent.delete_if{|m| m.length == 0 || m =~ /^#/}
-	mimecontent.each do |line|
-		value, ext = line.split(" ",2)
-			if (ext != nil)
-			key = ext.split(" ")
-			end
-		key.each do |c|
-		@mimetype[c] = value
-		end
-		end
-	
-	
-	    
+      @mimetype = Hash.new
+      mimecontent = mime_file_content.split("\n")
+      mimecontent.delete_if{|m| m.length == 0 || m =~ /^#/}
+      mimecontent.each do |line|
+        value, ext = line.split(" ",2)
+        if (ext != nil)
+          key = ext.split(" ")
+        end
+        key.each do |c|
+          @mimetype[c] = value
+        end
+      end
     end
-    
     # Returns the mime type for the specified extension
-	def for_extension(extension)
-	if (@mimetype[extension] == nil  )
-		"text/plain"
-		else
-		@mimetype[extension]
-	end
-	end
+    def for_extension(extension)
+      if (@mimetype[extension] == nil)
+        "text/plain"
+      else
+        @mimetype[extension]
+      end
     end
+  end
 end
