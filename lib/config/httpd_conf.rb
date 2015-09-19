ec84e83216c84133d07ba6b549db3f3f17ab41ce
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2015-09-19 16:40:13 -0700

    Fixed indentation and white space issues.

--- lib/config/httpd_conf.rb
+++ lib/config/httpd_conf.rb
@@ -4,68 +4,63 @@ require_relative 'configuration'
 module WebServer
   class HttpdConf < Configuration
     def initialize(httpd_file_content)
-	    @a = httpd_file_content
-	    # puts "input in httpd file #@a"
+        content.each_line do |line|
+        key = line.split(' ', 2)[0]
+        value = line.split(' ', 2)[1]
+        value.strip!
+        value.gsub!(/^\"|\"?$/, '')
+        #puts "Key Value pair"+key+":"+value
+        case key
+          when "Require"
+            @require_user = value
+          when "AuthUserFile"
+            @auth_user_file = value
+          when "AuthType"
+            @auth_type = value
+          when "AuthName"
+            @auth_name = value
+        end
+      end
     end
 
     # Returns the value of the ServerRoot
-    def server_root 
-	    "server_root/with/path"
+    def server_root
     end
 
     # Returns the value of the DocumentRoot
     def document_root
-	    "document_root"
     end
 
     # Returns the directory index file
     def directory_index
-	    "i.html"
     end
 
     # Returns the *integer* value of Listen
     def port
-	    1234
     end
 
     # Returns the value of LogFile
     def log_file
-	    "log_file"
     end
 
-    # Returns the name of the AccessFile 
+    # Returns the name of the AccessFile
     def access_file_name
-	    "access_file"
     end
 
     # Returns an array of ScriptAlias directories
     def script_aliases
-	    
     end
 
     # Returns the aliased path for a given ScriptAlias directory
     def script_alias_path(path)
-	    if (path == '/script_alias/')
-	    "script/alias/directory" 
-	    else
-		    nil 
-	    end
-	    
     end
 
     # Returns an array of Alias directories
     def aliases
-	    
-	    
     end
 
     # Returns the aliased path for a given Alias directory
     def alias_path(path)
-	   if (path == "/ab/")
-	    "alias/public_html/ab1/ab2/"
-	    else
-		    nil 
-	    end
     end
   end
 end
