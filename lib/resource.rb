9485ffec12d3609f787b6b91b4ded13b7b3a9c5f
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2015-09-20 15:53:06 -0700

    5 tests remain.

--- lib/resource.rb
+++ lib/resource.rb
@@ -7,5 +7,30 @@ module WebServer
       @conf = httpd_conf
       @mimes = mimes
     end
+
+    def resolve
+      uri = @request.uri
+      document_root = @conf.document_root
+      directory = ""
+      filename = ""
+      if (File.extname(uri) == "")
+        filename = @conf.directory_index
+        directory = uri
+      else
+        filename = uri.split("/").last
+        directory = (uri.split("/").first uri.split("/").size - 1).join("/")
+      end
+
+      #check for aliases
+      @conf.aliases.each do |a|
+        if directory.include? a
+          document_root = ""
+          directory =  directory.gsub(a, @conf.alias_path(a))
+        end
+      end
+
+      return document_root + directory + '/' + filename
+    end
+
   end
 end
